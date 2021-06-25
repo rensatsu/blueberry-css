cd11f83bba4a9bc1a20620a741bab29669a12383
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-06-25 16:51:27 +0300

    Update `gulp-sass`; format `gulpfile.js`

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const touch = require('gulp-touch-cmd');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const touch = require("gulp-touch-cmd");
 
-const sassFiles = [
-    'blueberry.scss',
-];
+const sassFiles = ["blueberry.scss"];
 
 async function styles() {
-    await sassFiles.forEach(async (src) => {
-        await gulp.src('scss/' + src)
-            .pipe(sass({
-                outputStyle: 'compressed'
-            }).on('error', sass.logError))
-            .pipe(gulp.dest('css/'))
-            .pipe(touch()) /* fix to update modified time */
-    });
-};
+  await sassFiles.forEach(async (src) => {
+    await gulp
+      .src("scss/" + src)
+      .pipe(
+        sass({
+          outputStyle: "compressed",
+        }).on("error", sass.logError)
+      )
+      .pipe(gulp.dest("css/"))
+      .pipe(touch()); /* fix to update modified time */
+  });
+}
 
 async function watch() {
-    await gulp.series('styles');
-    await gulp.watch('./scss/**/*.scss', gulp.series(styles));
-};
+  await gulp.series("styles");
+  await gulp.watch("./scss/**/*.scss", gulp.series(styles));
+}
 
 exports.watch = watch;
 exports.styles = styles;
